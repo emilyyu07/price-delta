31b09e0c43208c910670b3f2efe46c3bdfa80aee
Emily Yu <emilyyu60@gmail.com>
2026-04-19 20:12:14 -0400

    reinstalled node modules

--- frontend/src/hooks/useDemo.ts
+++ frontend/src/hooks/useDemo.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { DemoContext } from './DemoContext';
+import { DemoContext } from '../contexts/DemoContext';
 
 /**
  * Hook for consuming the demo context
